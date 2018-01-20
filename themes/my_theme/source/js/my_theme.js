776da0628e447e096c77d2723390b0df3ec82bb3
罗高 <luogao_lg@sina.com>
2018-01-20 15:28:20 +0800

    。。。

--- themes/my_theme/source/js/my_theme.js
+++ themes/my_theme/source/js/my_theme.js
@@ -2,29 +2,23 @@ $(function () {
     $('body')
         .on('click', '#menu-btn', function (e) {
             if ($(this).hasClass('active')) {
-                $('#full-screen-nav ul li')
-                    .each(function (index) {
-                        var el = $(this);
-                        setTimeout(function () {
-                            el.hide()
-                        }, index * 500)
+                $('#full-screen-nav')
+                    .fadeOut(200, function () {
+                        $('#full-screen-nav ul li').hide()
+                        $('#full-screen-nav').removeClass('active')
+                        $('#menu-btn').removeClass('active')
                     })
-                setTimeout(function () {
-                    $('#full-screen-nav').removeClass('active')
-                    $('#menu-btn').removeClass('active')
-                }, 2000);
             } else {
-                $('#full-screen-nav').addClass('active')
                 $(this).addClass('active')
-                setTimeout(function () {
+                $('#full-screen-nav').addClass('active').fadeIn(200, function () {
                     $('#full-screen-nav ul li')
                         .each(function (index) {
                             var el = $(this);
                             setTimeout(function () {
                                 el.show()
-                            }, index * 500)
+                            }, index * 200)
                         })
-                }, 500);
+                })
             }
 
         })
